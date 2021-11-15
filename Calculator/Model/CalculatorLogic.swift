b2630630a80da44f26ad3c4b41bcbfb15b785952
minjongkim <rlaalswhd200@naver.com>
2021-11-15 18:16:17 +0900

    made calculator with udemy

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,38 @@
+//
+//  CaculatorLogic.swift
+//  Calculator
+//
+//  Created by 김민종 on 2021/11/15.
+//  Copyright © 2021 London App Brewery. All rights reserved.
+//
+
+import Foundation
+ 
+
+struct CalculatorLogic {
+    
+    var number: Double
+    
+    init(number: Double) {
+        self.number = number
+    }
+
+    
+    
+    func calculate(symbol: String) -> Double? {
+        
+        
+            if symbol == "+/-" {
+                return number * -1
+            } else if symbol == "AC" {
+                return 0
+            } else if symbol == "%" {
+                return number * 0.01
+            }
+        
+        return nil
+                
+        }
+        
+    
+}
